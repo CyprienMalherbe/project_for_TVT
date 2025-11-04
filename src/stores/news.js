3319740465204c054738645bdd6ffa64463f1dd5
Cyprien Malherbe <cmalherbe@systra.com>
2025-11-04 16:30:20 +0100

    Marbella Duranteau

--- src/stores/news.js
+++ src/stores/news.js
@@ -3,6 +3,7 @@ import youngAquathlonImg from '@/assets/young-aquathlon.jpg'
 import eveilsImg from '@/assets/eveil.png'
 import bikeRunMousseaux2025 from '@/assets/bikerun-mousseaux-2025.jpg'
 import articleTriAmants2025 from '@/assets/article-tri-amants-2025.jpg'
+import marbellaDuranteau2025 from '@/assets/marbella-duranteau-2025.jpg'
 
 function slugify(text) {
   return text
@@ -44,6 +45,14 @@ export const useNewsStore = defineStore('news', {
         subtitle2: "Plus de 1000 participants sur le week-end, neuf courses lancées en aquathlon et en triathlon, 25 départements représentés, le Team Val’Eure Triathlon peut s’enorgueillir d’avoir mené à bien la plus grosse édition du Triathlon des 2 Amants depuis sa création en 2021.",
         subtitle3: 'Voir les résultats complets <a href="https://www.breizhchrono.com/detail-de-la-course/triathlondes2amants-minitricrosscourse22016a2019-2025-19009" target="_blank" rel="noopener noreferrer">ici</a>',
         published: new Date("2025-10-08T03:24:00"),
+        route: 'news'
+      },
+      {
+        title: 'CHAMPIONNAT DU MONDE IRONMAN 70.3 : TOUT UN CLUB DERRIÈRE NICO !',
+        image: marbellaDuranteau2025,
+        subtitle1: "Après une brillante qualification à Vichy et au delà, une saison 2025 de bout en bout solide, on y est : la préparation de plusieurs mois s'est bien passée, les bagages récupérés après une petite péripétie aéroportuaire, il ne reste plus à Nico Duranteau qu'à charger les batteries à bloc afin d' être au top dimanche pour sa dernière course de 2025, et quelle course !",
+        subtitle2: "Départ dimanche 09 novembre à partir de 8h. 📢 Go, Nico !",
+        published: new Date("2025-11-04T03:24:00"),
         route: 'news'
       },
     ],
